9c6c69c914a25e6038e6f8714a7bfdf5987b8619
paochange <paochange@gmail.com>
2021-07-11 14:38:02 +0800

    修改js02.js-增加九九乘法表(for迴圈)

--- js02.js
+++ js02.js
@@ -1,10 +1,19 @@
-function bmiCalculator(height, weight) {
-    // 實作內容
+// 計算公式是以體重（公斤）除以身高（公尺）的平方
+
+// function bmiCalculator(height, weight) {
+    實作內容
     let h = height / 100
-    let bmiCalculator = (weight / ( h * h ) )
-    return Math.floor(bmiCalculator *10)/10
-  //   math.floor(bmiCalculator) 
+    let w = weight
+    let bmiCalculator = w / ( h * h )
+    return Math.floor( bmiCalculator *10 ) / 10
   }
   
   // 身高 170 公分、體重 65 公斤
   console.log(bmiCalculator(170, 65))  // 印出 22.4
+
+  // 九九乘法表
+  // for (let i = 1; i<= 9; i = i + 1){
+  //   for (let j = 1; j <= 9; j = j + 1){
+  //     console.log(i + " x " + j + " = " + i * j)
+  //   }
+  // }
