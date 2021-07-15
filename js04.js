444e17e2528a22c1f5df5250e955bc981e51c6ca
paochange <paochange@gmail.com>
2021-07-15 10:12:55 +0800

    for迴圈練習

--- js04.js
+++ js04.js
@@ -4,12 +4,22 @@
 // 如果是 3 而且也是 5 的倍數，換成 'FizzBuzz'
 
 var list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
-for (let i = 0; i < list.length; i =i + 1){
+// for (let i = 0; i < list.length; i = i + 1){
+//     if (list[i] % 3 == 0 && list[i] % 5 != 0){
+//         list[i] = "Fizz"
+//     }else if (list[i] % 5 == 0 && list[i] % 3 != 0){
+//         list[i] = "Buzz"
+//     }else if (list[i] % 3 == 0 && list[i] % 5 == 0){
+//         list[i] = "FizzBuzz"
+//     }
+// }
+
+for (let i = 0; i < list.length; i = i + 1){
     if (list[i] % 3 == 0 && list[i] % 5 == 0){
         list[i] = "FizzBuzz"
-    }else if (list[i] % 3 == 0 && list[i] % 5 != 0){
+    }else if (list[i] % 3 == 0){
         list[i] = "Fizz"
-    }else if (list[i] % 5 == 0 && list[i] % 3 != 0){
+    }else if (list[i] % 5 == 0){
         list[i] = "Buzz"
     }
 }
