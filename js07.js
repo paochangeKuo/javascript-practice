ff8098fd847806dac9757804b15146ea51accc9f
paochange <paochange@gmail.com>
2021-07-28 06:34:14 +0800

    修改js07

--- js07.js
+++ js07.js
@@ -1,12 +1,15 @@
 const lists = [2, 4, 1, 8, 7, 3, 5]
 function greaterThan(n){
-    function select(x){
-      return x > n
-    }
-    return select
+  function bigerThan4(x){
+    return x > n
   }
+  return bigerThan4
+}
 // 實作...
 
 const result = lists.filter(greaterThan(4))
 console.log(result)  // 印出 [ 8, 7, 5 ]
+
+
+
 
