c80e51569dfa23bd4421a82ba1f8d94c25568e23
paochange <paochange@gmail.com>
2021-08-10 13:39:59 +0800

    修改js09,js10

--- js09.js
+++ js09.js
@@ -41,7 +41,6 @@ const isValidPassword = (password) => {
       if(w.includes(alphabet[j])) {
         return  true
       }
-    
     }
     return false
   }
@@ -52,3 +51,63 @@ console.log(isValidPassword("123passwordxyz"));  // false
 console.log(isValidPassword("helloworld"));      // false
 console.log(isValidPassword("Helloworld"));      // false
 console.log(isValidPassword("helloWorld2"));     // true
+
+
+
+
+// ----------------------------------
+
+const isValidPassword = (password) => {
+  // 規則：
+  // 1. 密碼至少要 8 個字
+  // 2. 密碼裡至少要有 1 個數字以及 1 個大寫英文字母
+  // 3. 密碼裡不能有 "password" 字樣
+  const number = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+  const capital = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  
+//     if (password.length >= 8){
+//       return true
+//     }else{
+//       return false
+//     }
+
+//     if (password.includes("password")){
+//       return false
+//     }
+  if (hasNumber(password)){
+    return true
+  }else{
+    return false
+  }
+  if (hasCapital(password)){
+    return true
+  }else{
+    return false
+  }
+
+  function hasNumber(n){
+    for(let i = 0; i < number.length; i++){
+      if (n.includes(number[i])){
+        return true
+      }
+    }
+    return false
+  }
+
+  function hasCapital(w){
+    for(let j = 0; j < capital.length; j++){
+      if (w.includes(capital[j])){
+        return true
+      }
+    }
+    return false
+  }
+  
+}
+console.log(isValidPassword("12345"));           // false
+console.log(isValidPassword("123passwordxyz"));  // false
+console.log(isValidPassword("helloworld"));      // false
+console.log(isValidPassword("Helloworld"));      // false
+console.log(isValidPassword("helloWorld2"));     // true
+
+
--- js10.js
+++ js10.js
@@ -1,16 +1,16 @@
-// var list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
-// let r = []
-// for(let i = 0; i <= list.length-1; i++){
-//     console.log(i)
-//   if(list[i] % 2 != 0){
-//       r.push(list[i])     
-//     }
-// }
-// console.log(r)
+let list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+let r = []
+for(let i = 0; i < list.length; i++){
+    // console.log(i)
+  if(list[i] % 2 != 0){
+      r.push(list[i])     
+    }
+}
+console.log(r)
 
 
 // ---------------------------------
-var list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+let list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
 let result = list.filter(number)
 function number(n){
     return (n % 2 != 0)
